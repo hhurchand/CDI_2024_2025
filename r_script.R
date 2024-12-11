97ce44e86be89f1936914232622f41a0505794be
AI-Aut-Sys <addingadoo.ashlry@yahoo.com>
2024-12-10 20:05:15 -0500

    The first Commit

---
+++ r_script.R
@@ -0,0 +1,12 @@
+
+# Name: AShley Addingadoo
+# DAte: 10 December 2024
+# Time: 7:47 pm
+
+
+# Import iris dataset from the file on your drive
+# Be careful "\", as you have to change it to "/" iin the filepath
+
+mydata <- read.csv("C:/Users/desk/Documents/Artificial Intelligence Specialist - LEA.E3/iris.csv")
+mydata
+
