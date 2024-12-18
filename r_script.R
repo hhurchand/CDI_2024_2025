82e48573a3e1a96df03a15387b7a3b0f759b2912
AI-Aut-Sys <addingadoo.ashlry@yahoo.com>
2024-12-17 21:55:30 -0500

    Add change to r_script

--- r_script.R
+++ r_script.R
@@ -85,3 +85,103 @@ trim_both
 trim_other_both <- str_squish ("          kerry     ")
 trim_other_both
 
+#################  DATA Filtering #########################
+
+#Using the Iris dataset
+# Selecting rows where column 1 is greater than 50
+
+dim(mydata)
+
+mydata_filtered <- subset(mydata,sepal.length > 5) # This is showing the col1 = "sepal.length" where the value is greater than 5, it will display.
+mydata_filtered
+
+mydata_filter1 <- mydata %>% subset(variety =="Setosa")
+mydata_filter1
+dim(mydata_filter1)
+
+library(dplyr) # how to import library in R studio
+# if the syntax don't work, you can go on Tools and install package
+
+# Filtering rows based on a condition
+# You have to import library "dplyr' in order to use this command
+
+mydata_filtered <- mydata%>%filter(sepal.length>5)
+mydata_filtered
+
+# Selecting Specific columns from a dataset
+
+mydata_selected <- mydata%>%select(sepal.width,petal.width)
+mydata_selected
+
+
+#################  DATA Sorting  #########################
+
+#  R provides various functions for data sorting such as order(), sort(), and arrange().
+
+# Sorting data based on column1 in ascending order
+mydata_sorted <- mydata[order(mydata$sepal.length),]
+mydata_sorted
+
+# Sorting data based on multiple columns
+# For this, you need to have the library "dplyr"  or install the package from Tools
+# Since we already install the package for previous exercise, we don't need to do it again
+
+mydata_sort <- mydata %>% arrange(sepal.length,petal.length)
+mydata_sort
+
+################# Teacher Example  #######################
+
+mean_petal_length <- mean(mydata_filter1$petal.length)
+mydata_mean <- mydata_filter1 %>% subset(petal.length>mean_petal_length)
+mydata_mean
+
+# Descending order sorting
+mydata_desc <- mydata_mean %>% arrange(desc(petal.length))
+mydata_desc
+
+
+# Summary
+
+#1 MAX: max(mydata$sepal.length)
+max(mydata$sepal.length)
+
+#2 MIN: min(mydata$sepal.length)
+min(mydata$sepal.length)
+
+#3 Mean : mean(mydata$sepal.length)
+mean(mydata$sepal.length)
+
+#4 Mode: doesnot exist, groupby
+
+#5 Median: median(mydata$sepal.length)
+median(mydata$sepal.length)
+
+#6 VAriance: var(mydata$sepal.length)
+var(mydata$sepal.length)
+
+#7 Standard deviation: 
+
+var_x <- var(mydata$sepal.length)
+
+sqrt(var_x)
+
+sd(mydata$sepal.length)
+
+#8 Lower quartile, Upper Quartile, InterQuartile
+
+#8a Upper_quartile <- quartile(mydata$sepal.length,0.75)
+Upper_quar <- quantile(mydata$sepal.length,0.75)
+Upper_quar
+
+#8b Lower_quartile <- quartile(mydata$sepal.length,0.25)
+Lower_quar <- quantile(mydata$sepal.length,0.25)
+Lower_quar
+
+#8c INterquartile = Upper quartile - lower quartile
+
+Inter_quar <- Upper_quar-Lower_quar
+Inter_quar
+
+
+
+
