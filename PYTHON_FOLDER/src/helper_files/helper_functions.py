93221c78ae72d8a3935fb919d1f8c64ddde5ada9
LenXons <showerlau@hotmail.com>
2025-01-29 22:16:41 -0500

    Add main and helper

---
+++ PYTHON_FOLDER/src/helper_files/helper_functions.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def sum_list(my_list):
+    return np.sum(my_list)
--- PYTHON_FOLDER/src/main.py
+++ PYTHON_FOLDER/src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from helper_files.helper_functions import sum_list
 
-print(np.sum([6,7,8]))
+print(sum_list([3,4,5]))
