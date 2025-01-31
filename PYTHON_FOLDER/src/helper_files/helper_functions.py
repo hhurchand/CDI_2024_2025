17080951851130b333cad7c557615dce5007b86b
LenXons <showerlau@hotmail.com>
2025-01-30 20:58:35 -0500

    Add helper.function

--- PYTHON_FOLDER/src/helper_files/helper_functions.py
+++ PYTHON_FOLDER/src/helper_files/helper_functions.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def sum_list(my_list):
     return np.sum(my_list)
+
+def average_list(my_list):
+    return np.mean(my_list)
--- PYTHON_FOLDER/src/main.py
+++ PYTHON_FOLDER/src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from helper_files.helper_functions import sum_list
+from helper_files.helper_functions import sum_list, average_list
 
 print(sum_list([3,4,5]))
+print(average_list([3,4,5]))
