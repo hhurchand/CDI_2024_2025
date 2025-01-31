0bc6c4399445356a45c5faa6daea62ac475d88eb
Pamelathanasoulias <pamela.athanasoulias@outlook.com>
2025-01-30 21:52:38 -0500

    My main.py

---
+++ PYTHON_FOLDER/src/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+from helper_files.helper_functions import sum_list
+from helper_files.helper_functions import average_list
+
+print(sum_list([6,7,8]))
+print(average_list([6,7,8]))
+
+
+
+
+
+
+
